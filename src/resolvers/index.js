90cdea52194877e486fda6832d9ea903174cd79a
Gweskaine <dianacris.tobon.m@gmail.com>
2021-12-06 11:53:22 -0500

    ultima correccion a nombres inscripcion

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,6 +1,6 @@
 const {proyectoResolvers} = require('./proyecto')
 const {usuarioResolvers} = require('./usuario')
-const {inscriptionResolvers} = require ('./inscription')
+const {inscriptionResolvers} = require ('./inscripcion')
 
 const resolvers = [proyectoResolvers, usuarioResolvers, inscriptionResolvers]
 
