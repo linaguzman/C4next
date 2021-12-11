78f2b8771135b61b08ba8a5cf216b785ace99c98
Gweskaine <90417903+Gweskaine@users.noreply.github.com>
2021-12-10 21:04:57 -0500

    Delete src directory

--- src/resolvers/index.js
+++
@@ -1,9 +0,0 @@
-const {proyectoResolvers} = require('./proyecto')
-const {usuarioResolvers} = require('./usuario')
-const {inscripcionResolvers} = require ('./inscripcion')
-const {avancesResolvers} = require('./avances')
-
-
-const resolvers = [proyectoResolvers, usuarioResolvers, inscriptionResolvers, avancesResolvers]
-
-module.exports = {resolvers}
